8a6f37c19f6d87c59c25246cc5d662c29dc58411
matthewjo <moon9goo@gmail.com>
2024-03-11 20:54:30 +0900

    commit - ChatWriterWidget 생성
    commit - ChatWriterWidget 생성
    클래스 생성만

---
+++ Source/PaintingGenie/Public/ChatWriterWidget.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChatWriterWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAINTINGGENIE_API UChatWriterWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	// BeginPlay 와 같은 기능의 함수
+	virtual void NativeConstruct() override;
+	
+};
