7be43b1533c05ebb2bcba27c0398ad81fdb14022
matthewjo <moon9goo@gmail.com>
2024-03-20 17:30:15 +0900

    Commit - 온라인서브시스템 초기 설정 (모듈추가, DefaultEngin.ini)
    Commit - 온라인서브시스템 초기 설정 (모듈추가, DefaultEngin.ini)
    
    1. 모듈 추가
    PaintingGenie.Build.cs 에
    "OnlineSubsystem", "OnlineSubsystemSteam"
    
    2. DefaultEngin.ini
    언리얼 문서의 내용 복사

--- Source/PaintingGenie/PaintingGenie.Build.cs
+++ Source/PaintingGenie/PaintingGenie.Build.cs
@@ -8,6 +8,8 @@ public class PaintingGenie : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Niagara"});
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", 
+			"UMG", "Niagara", "OnlineSubsystem", "OnlineSubsystemSteam"});
 	}
 }
