791f961afc12534f316492ed6a57d939686ab18f
cheezebee <koreapower5@naver.com>
2024-03-25 12:08:27 +0900

    컬러픽커 이전, 현재색깔

---
+++ Source/PaintingGenie/Public/BulletColorPickerWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BulletColorPickerWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAINTINGGENIE_API UBulletColorPickerWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<class UMaterial*> bulletColorPicker;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<class UImage*> bulletImageColor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<class FVector*> fcolorArray;
+	
+	
+};
